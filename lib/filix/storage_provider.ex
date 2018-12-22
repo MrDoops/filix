7c8318b7bc8c762c753ed4c68cb549cf42825d9f
Doops <zkwhited@gmail.com>
2018-12-22 12:26:16 -0700

    ..

--- lib/filix/storage_provider.ex
+++ lib/filix/storage_provider.ex
@@ -5,6 +5,8 @@ defmodule Filix.StorageProvider do
   alias Filix.Uploading.Commands.RequestUpload
 
   @callback request_upload(%RequestUpload{}) ::
-  {:ok, %{signed_url: String.t()}}
-  | {:error, String.t()}
+    {:ok, %{signed_url: String.t()}}
+    | {:error, String.t()}
+
+  # @callback delete_file()
 end
